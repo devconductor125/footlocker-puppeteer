c9cefeee5e1bbf73f4a42e1fa3323cf65b1325e6
devconductor125 <stresscontroller9@gmail.com>
2023-09-22 03:41:47 +0200

    sadasd

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,15 @@ require("dotenv").config()
 import puppeteer from 'puppeteer';
 import { ScrapedProduct, ScrapedProductSite } from "./types/ee"
 import { ProductType } from "./types/producttypes"
-import { Option } from "./types/option"
 import jsdom from "jsdom"
 const { JSDOM } = jsdom
 
 async function scrapePage(url: string, pagenum: number) {
-  const browser = await puppeteer.launch({headless: 'new'})
+  const browser = await puppeteer.launch({headless: 'new', executablePath: `C:/Program Files (x86)/Google/Chrome/Application/chrome.exe`})
   const page = await browser.newPage()
   let pageSlug = "?currentPage="+pagenum;
 
-  await page.goto(pageSlug)
+  await page.goto(url+pageSlug)
   await page.setViewport({width: 1080, height: 1024})
 
   console.log("Scraping product data...for ", pagenum, "th page")
@@ -56,7 +55,7 @@ async function scrapeItem(item: Element, productType: ProductType) {
   const itemPageLink = "https://www.footlocker.com"+itemLink.getAttribute("href");
 
   // Open new page
-  const browser = await puppeteer.launch({headless: 'new'})
+  const browser = await puppeteer.launch({headless: 'new', executablePath: `C:/Program Files (x86)/Google/Chrome/Application/chrome.exe`})
   const page = await browser.newPage()
   await page.goto(itemPageLink)
   await page.setViewport({width: 1080, height: 1024})
@@ -73,7 +72,7 @@ async function scrapeItem(item: Element, productType: ProductType) {
   const price = mainContent.querySelector('.ProductDetails-form__price').querySelector(".ProductPrice-final")?.textContent?.replace("$","")?.replace(",","");
 
   const detailContent = document.querySelector('#ProductDetails-tabs');
-  const description = detailContent.querySelector('#ProductDetails-tabs-details-panel').innerText;
+  const description = detailContent.querySelector('#ProductDetails-tabs-details-panel')?.innerText;
   const images = []
   
   if(!title || !url || !price || !images) {
@@ -92,31 +91,31 @@ async function scrapeItem(item: Element, productType: ProductType) {
   }
 
   // await DAO.storeProduct(product)
-  console.log(product);
+
   await page.close()
   await browser.close()
+  console.log("Scrapped product", product);
 }
 
 
 async function getAllPage(url: string) {
-  // const browser = await puppeteer.launch({
-  //   headless: 'new',
-  //   executablePath: `C:/Program Files (x86)/Google/Chrome/Application/chrome.exe`,
-  // });
-  // const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    executablePath: `C:/Program Files (x86)/Google/Chrome/Application/chrome.exe`,
+  });
+  const page = await browser.newPage();
 
-  // await page.goto(url, {
-  //   waitUntil: "load",
-  //   timeout: 0,
-  // });
-  // const data = await page.content()
-  // const dom = new JSDOM(data)
-  // const document = dom.window.document
-  // let nextButton = document.querySelectorAll("a[aria-label='Go to next page']");
-  // const lastPageElement = nextButton.parentElement.previousElementSibling;
-  // console.log("lastElement", lastPageElement)
-  // return parseInt(lastPageElement.innerText);
-  return 82;
+  await page.goto(url, {
+    waitUntil: "load",
+    timeout: 0,
+  });
+  const data = await page.content()
+  const dom = new JSDOM(data)
+  const document = dom.window.document
+  let nextButton = document.querySelectorAll("a[aria-label='Go to next page']");
+  const lastPageElement = nextButton.parentElement.previousElementSibling;
+  console.log("lastElement: ", lastPageElement)
+  return parseInt(lastPageElement.innerText);
 }
 
 async function scrapeProduct(url: string): Promise<void> {
